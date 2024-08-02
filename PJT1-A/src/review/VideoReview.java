61d70f7e4eee024c331761cfe2769cb9aceb8895
조홍균 <whghdrbs1002@gmail.com>
2024-08-02 16:40:49 +0900

    add VideoReview and VideoReviewDao

---
+++ PJT1-A/src/review/VideoReview.java
@@ -0,0 +1,22 @@
+package review;
+
+public class VideoReview {
+	// Review Class
+	// member field
+	private int videoNo;
+	private int reviewNo;
+	private String nickName;
+	private String content;
+	
+	// getters and setters
+	public int getVideoNo() {return videoNo;}
+	public void setVideoNo(int videoNo) {this.videoNo = videoNo;}
+	public int getReviewNo() {return reviewNo;}
+	public void setReviewNo(int reviewNo) {this.reviewNo = reviewNo;}
+	public String getNickName() {return nickName;}
+	public void setNickName(String nickName) {this.nickName = nickName;}
+	public String getContent() {return content;}
+	public void setContent(String content) {this.content = content;}
+	
+	
+}
