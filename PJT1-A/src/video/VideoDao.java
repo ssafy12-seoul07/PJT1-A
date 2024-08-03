4ded41e6d1910d86b0117e347f0371a08608b991
kihwn <cristine980@konkuk.ac.kr>
2024-08-03 13:27:05 +0900

    feat:add video 기능

--- PJT1-A/src/video/VideoDao.java
+++ PJT1-A/src/video/VideoDao.java
@@ -1,8 +1,64 @@
 package video;
 
 import java.util.List;
-public interface VideoDao {
-	List<Video> selectVideo();
-    Video selectVideoByNo(int no);
+import java.io.FileReader;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class VideoDao implements VideoDaoInterface {
+	private static VideoDao instance;
+	private List<Video> videoList;
+	private static final int MAX_SIZE = 100;
+	private int size;
+	private static Gson gson;
+	
+	private VideoDao() {
+		gson = new Gson();
+		try (FileReader reader = new FileReader("videos.json")) {
+            Type listType = new TypeToken<List<Video>>() {}.getType();
+            videoList = gson.fromJson(reader, listType);
+            size = videoList.size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static VideoDao getInstance() {
+		if (instance == null) {
+            instance = new VideoDao();
+        }
+        return instance;
+	}
+	
+	@Override
+    public List<Video> selectVideo() {
+        return videoList;
+    }
+    
+	@Override
+    public Video selectVideoByNo(int no) {
+    	for (Video video: videoList) {
+    		if (video.getNo()==no) {
+    			return video;
+    		}
+    	}
+    	return null;
+    }
+	
+	public void loadFromJsonFile(String filename) {
+        try (FileReader reader = new FileReader(filename)) {
+            Type listType = new TypeToken<List<Video>>() {}.getType();
+            videoList = gson.fromJson(reader, listType);
+            size = videoList.size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+	
 
 }
